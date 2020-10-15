7badd78c4e6787507a200af4de09f73a5caed960
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-15 12:46:08 +0900

    空のメッセージが表示されないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,13 +6,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    room = Room.find(params[:room_id])
-    message = room.messages.new(message_params)
-    if message.save
-      redirect_to room_messages_path(room)
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
     else
-      @messages = room.messages.includes(:user)
-      redirect :index
+      @messages = @room.messages.includes(:user)
+      render :index
     end
   end
 
